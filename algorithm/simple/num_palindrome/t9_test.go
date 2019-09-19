bb2c61d350432c52744fd92ecb531db4241d9c5c
Qianlu <Qianlu110013>
2019-09-19 16:21:45 +0800

    1.t9 palindrome num

---
+++ algorithm/simple/num_palindrome/t9_test.go
@@ -0,0 +1,21 @@
+/*
+#
+# @Time : 2019/9/19 15:50
+# @Author : Qian Lu
+*/
+
+package num_palindrome
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_IsPalindrome(t *testing.T) {
+
+	fmt.Println(isPalindrome(12321))
+	fmt.Println(isPalindrome(1221))
+	fmt.Println(isPalindrome(10))
+	fmt.Println(isPalindrome(-121))
+
+}
